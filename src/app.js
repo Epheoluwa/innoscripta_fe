e81468b3376cb2640a931bce5f1fd13521b037e4
Solomon sunmola <sunmolasolomon@gmail.com>
2023-06-22 23:23:24 +0100

    removing jungs

--- src/app.js
+++ src/app.js
@@ -6,13 +6,7 @@ import "assets/scss/now-ui-kit.scss?v=1.5.0";
 import "assets/demo/demo.css?v=1.5.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
 // pages for this kit
-import Index from "views/Index.js";
 import Home from "views/Home";
-import NucleoIcons from "views/NucleoIcons.js";
-import LoginPage from "views/examples/LoginPage.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import IndexNavbar from 'components/Navbars/IndexNavbar';
 import Login from 'views/Auth/Login';
 import Register from 'views/Auth/Register';
 import Protected from 'Utils/Protected';
@@ -35,13 +29,6 @@ const app = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
 
-                {/* remove this part later */}
-                <Route path="/index" element={<Index />} />
-
-                <Route path="/nucleo-icons" element={<NucleoIcons />} />
-                <Route path="/landing-page" element={<LandingPage />} />
-                <Route path="/profile-page" element={<ProfilePage />} />
-                <Route path="/login-page" element={<LoginPage />} />
 
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
